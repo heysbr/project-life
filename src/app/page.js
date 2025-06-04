036a95d6666380a76815ab1efeb728b3579be8be
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-06-05 01:48:25 +0530

    tried for mobile view

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,12 @@ import LoginForm from "../components/LoginForm/LoginForm";
 
 export default function Home() {
   return (
-    <div className="flex h-[64%] bg-white w-7/10 drop-shadow-2xl/25 drop-shadow-[#f95b85] rounded-lg">
-      <div className="flex flex-col justify-center items-center font-lato h-full w-1/2">
-        <p className="font-semibold  text-2xl lg:text-3xl text-[#FC5285] mb-8">“Labour Care Guide”</p>
+    <div className="flex flex-col mt-25 lg:m-0 lg:flex-row h-full lg:h-[64%] bg-white w-7/10 drop-shadow-2xl/25 drop-shadow-[#f95b85] rounded-lg">
+      <div className="hidden lg:flex flex-col justify-center items-center font-lato h-full w-1/2">
+        <p className="font-semibold text-2xl lg:text-3xl text-[#FC5285] mb-8">“Labour Care Guide”</p>
         <Image src={loginLady} alt="" className="size-80" priority />
       </div>
-      <div className=" text-2xl h-full w-1/2 ">
+      <div className="w-full lg:w-1/2">
         <LoginForm />
       </div>
     </div>
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -5,12 +5,14 @@ import eye from "../images/pass-eye.svg";
 
 export default function LoginForm() {
   return (
-    <div className="w-3/4 h-fit p-6 bg-white rounded-2xl mt-20">
-      <h2 className="font-semibold text-2xl lg:text-3xl text-[#FC5285]">Login</h2>
+    <div className="w-3/4 h-fit p-6 bg-white rounded-2xl mt-4 lg:mt-20 mx-auto">
+      <h2 className="font-semibold text-2xl lg:text-3xl text-[#FC5285]">
+        Login
+      </h2>
       <form className="mt-8">
         {/* Email  */}
         <div className="mb-4">
-          <label  className="block text-sm font-medium text-gray-700">
+          <label className="block text-sm font-medium text-gray-700">
             Email ID
           </label>
           <input
@@ -24,32 +26,44 @@ export default function LoginForm() {
         </div>
         {/* Password  */}
         <div className="mb-4">
-          <label  className="block text-sm font-medium text-gray-700">
+          <label className="block text-sm font-medium text-gray-700">
             Password
           </label>
 
           <div className="relative ">
-
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-            className="w-full px-4 py-2  border border-gray-300 rounded-md  focus:ring-blue-500 focus:border-blue-500 placeholder-[#BEC5D3] text-sm"
-            required
-            /><Image src={eye} alt="eye"
-            className="absolute top-1/2 -translate-y-1/2 right-3 "
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              className="w-full px-4 py-2  border border-gray-300 rounded-md  focus:ring-blue-500 focus:border-blue-500 placeholder-[#BEC5D3] text-sm"
+              required
             />
-            </div>
-
-
+            <Image
+              src={eye}
+              alt="eye"
+              className="absolute top-1/2 -translate-y-1/2 right-3 "
+            />
+          </div>
         </div>
         {/* Forget Password  */}
         <div className="flex justify-end mb-4">
-          <a href="#" className="text-sm text-[#52575C] font-semibold hover:underline"> Forgot password? </a>
+          <a
+            href="#"
+            className="text-sm text-[#52575C] font-semibold hover:underline"
+          >
+            {" "}
+            Forgot password?{" "}
+          </a>
         </div>
         {/* Login Button */}
-        <button type="submit" className="w-full px-4 py-2 text-white bg-[#FC5285] rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 text-sm" > Login </button>
+        <button
+          type="submit"
+          className="w-full px-4 py-2 text-white bg-[#FC5285] rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 text-sm"
+        >
+          {" "}
+          Login{" "}
+        </button>
       </form>
     </div>
   );
