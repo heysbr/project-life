903d16c798c2d64f72cb9e0b9d33b91cc586b96a
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-06-05 09:48:15 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-row h-full lg:h-[64%] bg-white w-7/10 drop-shadow-2xl/25 drop-shadow-[#f95b85] rounded-lg">
       <div className="hidden lg:flex flex-col justify-center items-center font-lato h-full w-1/2">
-        <p className="font-semibold text-2xl lg:text-3xl text-[#FC5285] mb-8">“Labour Care Guide” Hello</p>
+        <p className="font-semibold text-2xl lg:text-3xl text-[#FC5285] mb-8">“Labour Care Guide”</p>
         <Image src={loginLady} alt="" className="size-80" priority />
       </div>
       <div className="w-1/2">
