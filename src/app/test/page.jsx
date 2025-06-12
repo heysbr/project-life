36b65460c477145a3200e3ff5ab4b30bd251df96
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-06-12 11:59:43 +0530

    added Modal dumpy

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,12 +1,7 @@
-import Checkbox from '@/components/newForm/Checkbox'
-import Datepicker from '@/components/newForm/Datepicker'
-import Email from '@/components/newForm/Email'
-import Form from '@/components/newForm/Form'
-import Mobile from '@/components/newForm/Mobile'
-import Radiobutton from '@/components/newForm/Radiobutton'
-import Select from '@/components/newForm/Select'
-import Textfield from '@/components/newForm/Textfield'
-import React from 'react'
+"use client"
+import Modal from '@/components/Modal/Modal'
+
+import React, { useState } from 'react'
 
 
 const options = [
@@ -18,22 +13,12 @@ const options = [
 const radioGroup = [{ value: "Male" }, { value: "Female" }];
 
 export default function page() {
-  return (<form>
-  <Textfield placeholder={"Enter First Name "}>First Name</Textfield>
-  <Select options={options}>Select</Select>
-  <Checkbox>text for checkbox</Checkbox>
-  <Radiobutton radioGroup={radioGroup}>Gender</Radiobutton>
-    <Datepicker>Date of Birth</Datepicker>
-    <Email/>
-    <Mobile/>
+  const [toggle, setToggle] = useState(false)
+  function handleToggle(){
+    setToggle(!toggle);
+  }
 
-
-
-
-
-
-    <input type='submit' className='absolute bottom-0 right-0 m-10 border bg-gray-800 text-white px-10 py-3 rounded-3xl  hover:bg-gray-700'/>
-  </form>  
+  return ( <Modal />
   )
 }
 
