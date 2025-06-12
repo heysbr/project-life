df4a7b77e46ecc97c9c5f1b27cf924c2f8d0ea01
Abhimanyu Kushwaha <88226869+abhimanyu-kushwaha@users.noreply.github.com>
2025-06-12 15:32:37 +0530

    form component added

--- src/components/comman/FormField.jsx
+++ src/components/comman/FormField.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-const FormField =()=>{
-    return (
-        <>
-        this is a form field component
-        </>
-    )
+import React from "react";
+import FormTextField from "./FormField/FormTextField";
+import FormRadioField from "./formField/FormRadioField";
+const FormField = (props) => {
+    console.log("props-formfield", props);
+  const renderField = () => {
+    switch (props?.type) {
+      case "radio":
+        return <FormRadioField {...props} />;
+        case "password":
+            return <FormTextField {...props} />
+      default:
+        return <FormTextField {...props} />;
+    }
+  };
+  return <>{renderField()}</>;
 };
 
 export default FormField;
--- src/components/comman/formField/FormTextField.jsx
+++ src/components/comman/formField/FormTextField.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import eye from "@/components/images/pass-eye.svg";
 import Image from "next/image";
 const FormTextField = (props) => {
+    console.log("props-formtextfield", props);
   return (
     <>
       <div className="relative ">
         <label className="text-sm font-medium text-gray-700 mb-2">
-          {props?.children}
+          {props?.label}
         </label>
         <input
           type={props?.pass ? "password" : "text"}
           id={props?.name}
           name={props?.name}
-          placeholder={placeholder}
+          placeholder={props?.placeholder}
           className="w-full px-4 py-2  border border-gray-300 rounded-md   placeholder-[#BEC5D3] text-sm focus:outline-0"
           required
         />
