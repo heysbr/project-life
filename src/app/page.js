b4b3b2db014f312cb91019a14af76d345579dba9
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-06-13 12:50:56 +0530

    style: update Home component layout with background images and logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,36 @@
 import Button from "@/components/Forms/Button";
+import svg1 from "@/components/images/bg-svg1.svg";
+import svg2 from "@/components/images/bg-svg2.svg";
+import logo from "@/components/images/logo.svg";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-pink-50">
-      <Button label="Get Started" link="/login"/>
+    
+    <div className={`bg-[#FFFBFB]`}>
+      <Image
+        src={svg1}
+        alt=""
+        className="absolute left-11 top-11/16 overflow-hidden"
+      />
+      <Image
+        src={svg2}
+        alt=""
+        className="absolute right-11 down-11/16 overflow-hidden"
+      />
+      <Link
+        href={"/"}
+      >
+        <Image
+          src={logo}
+          alt=""
+          className="absolute left-45  overflow-hidden size-30 "
+        />
+      </Link>
+      <div className="flex flex-col justify-center items-center h-screen w-full ">
+        <Button label="Get Started" link="/login"/>
+      </div>
     </div>
   );
 }
