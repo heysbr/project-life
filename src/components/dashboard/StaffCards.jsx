90a8d80af9ac33f257550918fd063646edf1e281
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-06-24 15:21:43 +0530

    feat: refactor StaffCards component to use StaffCard subcomponent and enhance UI with toggle menu

--- src/components/dashboard/StaffCards.jsx
+++ src/components/dashboard/StaffCards.jsx
@@ -17,82 +17,21 @@ export default function StaffCards({ data = [] }) {
 
   return (
     <div className="mb-10 ">
-      <div className="grid grid-cols-3 gap-4 ">
-        {currentData.map((staff, i) => {
-          const active = staff?.active ?? false;
-          return (
-            <div
-              key={staff.id || i}
-              className="relative max-w-md rounded-2xl shadow-md bg-white overflow-hidden"
-            >
-              <div className="relative h-20 bg-pink-100 px-4 py-3 flex items-center justify-between">
-                <Image src={Profile} alt="profile" />
-                <div className="flex-1 mx-5">
-                  <h2 className="text-lg font-bold">{staff.name || "N/A"}</h2>
-                  <p className="text-sm text-gray-500">
-                    ID- {staff.id || "N/A"}
-                  </p>
-                  <Image
-                    src={Call}
-                    className="absolute right-3 bottom-2.5"
-                    alt="call"
-                  />
-                  <Image
-                    src={Menu}
-                    className="absolute right-3 top-2.5"
-                    alt="menu"
-                  />
-                </div>
-              </div>
 
-              <table className="m-5">
-                <tbody>
-                  <tr>
-                    <td className="h-8 w-3/7">Email</td>
-                    <td className="text-[#7D8491]">{staff.email || "N/A"}</td>
-                  </tr>
-                  <tr>
-                    <td className="h-8 w-3/7">Phone No.</td>
-                    <td className="text-[#7D8491]">{staff.phoneNo || "N/A"}</td>
-                  </tr>
-                  <tr>
-                    <td className="h-8 w-3/7">Profession</td>
-                    <td className="text-[#7D8491]">
-                      {staff.profession || "N/A"}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="h-8 w-3/7">Total Patient</td>
-                    <td className="text-[#7D8491]">
-                      {staff.totalPatient ?? "N/A"}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="h-8 w-3/7">Initials</td>
-                    <td className="text-[#7D8491]">
-                      {staff.initials || "N/A"}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <span
-                className={`absolute bottom-3 font-semibold right-6 ${
-                  active ? "text-[#2DB23B]" : "text-[#ABAFB7]"
-                }`}
-              >
-                &#8226;{active ? "  On Duty" : "  Off Duty"}
-              </span>
-            </div>
-          );
-        })}
+      {/* Cards */}
+      <div className="grid grid-cols-3 gap-4 ">
+        {currentData.map((staff, i) => <StaffCard staff={staff} i={i} key={i} />)}
       </div>
+
       {/* Pagination Controls */}
       {totalPages > 1 && (
         <div className="flex justify-end mt-6 space-x-2">
+
+          {/* Previous Butoon */}
           <button
             onClick={() => setPage((p) => Math.max(1, p - 1))}
             disabled={page === 1}
-            className="px-3 py-1 rounded border border-[#E8E8E8] disabled:opacity-50 transition-all duration-300"
+            className="px-3 py-1 rounded border border-[#E8E8E8] disabled:opacity-50"
           >
             <svg
               width="16"
@@ -109,17 +48,23 @@ export default function StaffCards({ data = [] }) {
               </g>
             </svg>
           </button>
+
+          {/* Numbers Button */}
           {Array.from({ length: totalPages }, (_, idx) => (
             <button
               key={idx}
               onClick={() => setPage(idx + 1)}
               className={`px-3 py-1 rounded ${
-                page === idx + 1 ? "bg-[#FC5285] text-white" : "border-[#E8E8E8] border"
+                page === idx + 1
+                  ? "bg-[#FC5285] text-white"
+                  : "border-[#E8E8E8] border"
               }`}
             >
               {idx + 1}
             </button>
           ))}
+
+          {/* Next Button */}
           <button
             onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
             disabled={page === totalPages}
@@ -138,8 +83,95 @@ export default function StaffCards({ data = [] }) {
               />
             </svg>
           </button>
+
         </div>
       )}
+    </div>
+  );
+}
+
+function StaffCard({ staff, i }) {
+  const [toggle, setToggle] = useState(false)
+  const active = staff?.active ?? false;
+  return (
+    <div
+      key={staff.id || i}
+      className={`relative max-w-md rounded-2xl shadow-md bg-white overflow-hidden ${
+        active ? "opacity-100" : "opacity-50"
+      } transition-all duration-300 `}
+    >
+      {/* Card Head */}
+      <div className="relative h-20 bg-pink-100 px-4 py-3 flex items-center justify-between">
+        <Image src={Profile} alt="profile" />
+        <div className="flex-1 mx-5">
+          <h2 className="text-lg font-bold">{staff.name || "N/A"}</h2>
+          <p className="text-sm text-gray-500">ID- {staff.id || "N/A"}</p>
+          <Image
+            src={Call}
+            className="absolute right-3 bottom-2.5"
+            alt="call"
+          />
+          <Image
+            src={Menu}
+            className="absolute right-3 top-2.5 cursor-pointer"
+            onClick={()=>setToggle(!toggle)}
+            alt="menu"
+          />
+
+          {/* Edit Menu */}
+          {toggle && (
+            <div className="absolute flex list-disc list-inside w-1/2 top-9 right-2 text-sm bg-white shadow-lg rounded-md p-4 z-10 transition-all duration-500"
+            onClick={()=>setToggle(!toggle)}>
+              <ul className="list-disc marker:text-[#FC5285] list-outside ml-5">
+                <li className="p-1 rounded-md hover:bg-gray-100 cursor-pointer">
+                  Edit Staff
+                </li>
+                <li className="p-1 rounded  hover:bg-gray-100 cursor-pointer">
+                  Make an admin
+                </li>
+                <li className="p-1 rounded hover:bg-gray-100 cursor-pointer">
+                  Remove from institution
+                </li>
+              </ul>
+            </div>
+          )}
+
+        </div>
+      </div>
+      
+      {/* Card Body */}
+      <table className="m-5">
+        <tbody>
+          <tr>
+            <td className="h-8 w-3/7">Email</td>
+            <td className="text-[#7D8491]">{staff.email || "N/A"}</td>
+          </tr>
+          <tr>
+            <td className="h-8 w-3/7">Phone No.</td>
+            <td className="text-[#7D8491]">{staff.phoneNo || "N/A"}</td>
+          </tr>
+          <tr>
+            <td className="h-8 w-3/7">Profession</td>
+            <td className="text-[#7D8491]">{staff.profession || "N/A"}</td>
+          </tr>
+          <tr>
+            <td className="h-8 w-3/7">Total Patient</td>
+            <td className="text-[#7D8491]">{staff.totalPatient ?? "N/A"}</td>
+          </tr>
+          <tr>
+            <td className="h-8 w-3/7">Initials</td>
+            <td className="text-[#7D8491]">{staff.initials || "N/A"}</td>
+          </tr>
+        </tbody>
+      </table>
+      
+      <span
+        className={`absolute bottom-3 font-semibold right-6 ${
+          active ? "text-[#2DB23B]" : "text-[#ABAFB7]"
+        }`}
+      >
+        &#8226;{active ? "  On Duty" : "  Off Duty"}
+      </span>
     </div>
   );
 }
